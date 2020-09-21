6521983751747b0c3cdb246492acfaba84767ef0
Pablo Domínguez Durán <pablo.mdd.007@gmail.com>
2020-09-21 17:31:08 -0500

    subset with backtracking

--- Leetcode/subsets.js
+++ Leetcode/subsets.js
@@ -4,19 +4,46 @@
  * @return {number[][]}
  */
 //  CASCADING
-var subsets = function(nums) {
-    const output = [[]];
+// var subsets = function(nums) {
+//     const output = [[]];
     
-    for(let num of nums) {
-        const newSubsets = [];
-        for(let curr of output) {
-            newSubsets.push([...curr, num]);
+//     for(let num of nums) {
+//         const newSubsets = [];
+//         for(let curr of output) {
+//             newSubsets.push([...curr, num]);
+//         }
+        
+//         // for(let curr of newSubsets) {
+//         //     output.push(curr);
+//         // }
+//         output.push(...newSubsets);
+//     }
+//     return output;
+// };
+
+// BACKTRACKING 
+var subsets = function(nums) {
+    let k;
+    const output = [];
+    
+    const backtrack = (first = 0, curr = []) => {
+        // if subset is complete
+        if(curr.length === k){
+            output.push([...curr])
         }
-        /*
-        for(let curr of newSubsets) {
-            output.push(curr);
-        }*/
-        output.push(...newSubsets);
+        
+        for(let i = first; i < nums.length; i++) {
+            curr.push(nums[i]);
+            backtrack(i + 1, curr);
+            // backtrack
+            curr.pop();
+            
+        }
+    }
+    
+
+    for(k = 0; k < nums.length + 1; k++){
+        backtrack();
     }
     return output;
 };
