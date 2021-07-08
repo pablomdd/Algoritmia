eb8fcba8ad98d3c9c5904bdc1e7f9f9e5dfe032a
pablomdd <pablo.mdd.007@gmail.com>
2021-07-07 23:22:31 -0500

    add inverTree and isSameTree in python

---
+++ Leetcode/BinaryTrees/invertTree.py
@@ -0,0 +1,18 @@
+# 226. Invert Binary Tree
+# https://leetcode.com/problems/invert-binary-tree/submissions/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if(not root):
+            return
+
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
