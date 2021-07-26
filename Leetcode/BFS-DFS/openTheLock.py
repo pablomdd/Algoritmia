678369bfa227ed022c150d1453be0c0e96161836
pablomdd <pablo.mdd.007@gmail.com>
2021-07-25 23:03:06 -0500

    add open the lock space analysis

--- Leetcode/BFS-DFS/openTheLock.py
+++ Leetcode/BFS-DFS/openTheLock.py
@@ -32,11 +32,26 @@ class Solution:
                 for neighboor in getNeighboors(curr):
                     if(neighboor in visited):
                         continue
-# Adding neigboor node to visited here is important, because nodes at the same level would be considered all over again otherwise, causing a loop
+# Adding neigboor node to visited here is important,
+# because nodes at the same level would be considered all over again
+# otherwise, causing a loop
                     visited.add(neighboor)
                     queue.append(neighboor)
             turns += 1
         return -1
+# Time Linear. O(n)
+# Space O(deadset)->~O(1) + O(queue) -> O(n???) + O(visited) -> O(n) = O(n)
+# The space for the queue at least for this problem, is at most (8*H) where H is the height
+# of the bfs tree that tends to be ~exponential~
+# BFS tree
+#  0 -> root = "0000"
+#  8 -> the 8 childs of root and so on...
+# 8**2
+# ...
+# 8**H -> O(8**H)
+# In this case though the mem grows exponentially, it does have a limit, ie, a fixed num of combinations
+# Nums of combs: 10! in 4 -> 10P4 = 10!/(10-4)!
+# Sooo. queue mem does grow for the input but up to a const??? So... it's constant?
 
 
 # BFS - first ocurrence is the shorted path
