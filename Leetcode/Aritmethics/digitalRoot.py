85679fef35f01b423307175d2bd3b471940beb47
pablomdd <pablo.mdd.007@gmail.com>
2021-09-07 21:21:18 -0500

    add digital root py

---
+++ Leetcode/Aritmethics/digitalRoot.py
@@ -0,0 +1,29 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        res = 0
+        while(True):           # 38
+            #           get sum of digits
+            while(num > 0):         # 11
+                res += num % 10
+                num = num // 10
+            if(res < 10):
+                break
+            else:
+                num = res
+                res = 0
+        return res
+
+# 1234
+# 10
+# 1
+
+# 38
+# 11
+
+# class Solution:
+#     def addDigits(self, num: int) -> int:
+#         if num == 0:
+#             return 0
+#         if num % 9 == 0:
+#             return 9
+#         return num % 9
