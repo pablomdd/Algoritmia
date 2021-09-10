2b8dec448dfc8ab7b47685aea22bc9bb5656fe71
pablomdd <pablo.mdd.007@gmail.com>
2021-09-09 21:56:48 -0500

    add hamming distance

---
+++ Leetcode/Aritmethics/hammingDistance.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        hamm = x ^ y
+        res = 0
+        while(hamm > 0):
+            res += hamm % 2
+            hamm //= 2
+        return res
